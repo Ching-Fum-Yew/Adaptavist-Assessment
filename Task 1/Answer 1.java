a1b478a428daf75324b3c06948ec8f3f8bfa01d3
Ching-Fum-Yew <41646598+Ching-Fum-Yew@users.noreply.github.com>
2024-03-25 13:17:36 +0800

    Update Answer 1.java

--- Task 1/Answer 1.java
+++ Task 1/Answer 1.java
@@ -1,1 +1,14 @@
+// **********Answer 1**********
+//Number field for number of cat
+def cat = 50
+return cat
 
+//Number field for number of dog
+def dog = 20
+return dog
+
+//Calculate total
+def dog = getCustomFieldValue("Number of dog") as Integer
+def cat = getCustomFieldValue("Number of cat") as Integer
+def total = dog + cat
+return total
